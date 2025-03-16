fc9015ab7502affc4679b99b6b1c8cd4b3b53704
Хасан Хамраев <khasanknyazev81@gmail.com>
2025-03-16 13:56:44 +0300

    Create sniffer

---
+++ src/sniffer.py
@@ -0,0 +1,26 @@
+import os
+
+from scapy.all import sniff, Raw, ifaces
+from scapy.layers.http import HTTPRequest, HTTP
+from scapy.layers.inet import IP, TCP
+from scapy.sessions import TCPSession
+from scapy.packet import Packet
+
+
+def sniffing_firewall(packet: Packet) -> None:
+    if packet.haslayer(HTTPRequest):
+        print(packet)
+
+
+def main() -> None:
+    iface = ifaces.dev_from_index(1)
+    sniff(
+        prn=sniffing_firewall,
+        iface=iface,
+        filter='tcp port 80',
+        session=TCPSession
+    )
+
+
+if __name__ == '__main__':
+    main()
